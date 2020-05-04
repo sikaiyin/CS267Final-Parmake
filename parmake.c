7f549d3b325171048506f998da9046a5d01a2129
MichaelSky <30276683+sikaiyin@users.noreply.github.com>
2020-05-04 15:15:54 -0700

    234

--- parmake.c
+++ parmake.c
@@ -1,293 +1,254 @@
-/**
- * Parallel Make
- * CS 241 - Fall 2016
- */
-#include <string.h>
-#include <ctype.h>
-#include <sys/types.h>
+#include <pthread.h>
 #include <sys/stat.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <time.h>
 #include <unistd.h>
+#include <omp.h>
+#include "format.h"
+#include "graph.h"
 #include "parmake.h"
 #include "parser.h"
 #include "queue.h"
-#include "vector.h"
-// #include <pthread.h>
-#include "rule.h"
+#include "set.h"
 
-int targetcount = 0;
-//pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
+set *cycled_set;
+set *task_set;
+vector *task_vector;
+graph *file_graph;
+int new_changes_in_dependencies;
+//pthread_mutex_t m;
+//pthread_mutex_t n;
 
-Vector * v;
-Vector * circular;
-void *mycopy(void *elem) {
-  return (void*)elem;
+omp_lock_t *m;
+omp_lock_t *n;
+
+pthread_cond_t cv;
+pthread_cond_t cu;
+
+// return value: 1 means cycle detected, 0 means no cycle detected
+int detect_cycle(char *curr_target) {
+    if (set_contains(cycled_set, curr_target)) {
+        return 1;
+    }
+
+    set_add(cycled_set, curr_target);
+
+    vector *dependencies = graph_neighbors(file_graph, curr_target);
+    for (size_t i = 0; i < vector_size(dependencies); i++) {
+        char *next_target = (char *)vector_get(dependencies, i);
+        int have_cycle = detect_cycle(next_target);
+        if (have_cycle) {
+            vector_destroy(dependencies);
+            return 1;
+        }
+    }
+    vector_destroy(dependencies);
+    set_remove(cycled_set, curr_target);
+
+    if (!set_contains(task_set, curr_target)) {
+        rule_t *rule = (rule_t *)graph_get_vertex_value(file_graph, curr_target);
+        rule->data = (void *)graph_vertex_degree(file_graph, curr_target);
+        set_add(task_set, curr_target);
+    }
+
+    return 0;
 }
 
-void mydes(void *elem) {
-  free(elem);
-  return;
+void change_antineighbor_data(char *curr_target) {
+    vector *curr_antineighbors = graph_antineighbors(file_graph, curr_target);
+    omp_set_lock(&n[0]);
+    for (size_t i = 0; i < vector_size(curr_antineighbors); i++) {
+        char *curr_antineighbor = (char *)vector_get(curr_antineighbors, i);
+        rule_t *curr_antineighbor_rule = (rule_t *)graph_get_vertex_value(file_graph, curr_antineighbor);
+        curr_antineighbor_rule->data = (void *)(((size_t)(curr_antineighbor_rule->data)) - 1);
+    }
+    new_changes_in_dependencies = 1;
+    pthread_cond_broadcast(&cu);
+    omp_unset_lock(&n[0]);
+    vector_destroy(curr_antineighbors);
+    free(curr_target);
 }
 
-queue_t * ruleq;
+// state: -1 failed, 0 untouched, 1 satisfied
+void *run_makefile(void *ptr) {
+    while (1) {
+        int no_more_task = 0;
+        int waiting_for_ready_target = 1;
+        char *curr_target = NULL;
 
-void parsed_new_target(rule_t* t) {
-  if(t==NULL) {
-    return;
-  }
-  queue_push(ruleq, (void*)t);//push all the rule_t to a queue
-  Vector_append(v, (void*)t);
-  targetcount++;
-  return;
-}
-
-void * thread_function(void * id) {
-  *(int*)id = 1;
-  while(1) {
-    //pthread_mutex_lock(&m);
-    //if(targetcount<=0) {
-    //  pthread_mutex_unlock(&m);
-    //  break;
-    //}
-    //pthread_mutex_unlock(&m);
-    rule_t * r = (rule_t*)queue_pull(ruleq);
-    size_t y = 0;
-    int shouldreturn = 0;
-    for(y=0;y<Vector_size(circular);y++) {
-      rule_t * wa = Vector_get(circular,y);
-      if(strcmp(wa->target,r->target)==0) {
-        shouldreturn = 1;
-      }
-    }
-    if(r->state!=0) {
-      queue_push(ruleq, (void*)r);
-    }
-    else if(shouldreturn == 1) {
-      r->state = -1;
-      //pthread_mutex_lock(&m);
-      queue_push(ruleq, (void*)r);
-      targetcount--;
-      //pthread_mutex_unlock(&m);
-    }
-    else {
-      if(Vector_size(r->dependencies)==0) {
-        Vector* temp = r->commands;
-	if(access(r->target,F_OK)==0) {
-	  r->state = 1;
-          //pthread_mutex_lock(&m);
-          queue_push(ruleq, (void*)r);
-          targetcount--;
-          //pthread_mutex_unlock(&m);
-	}
-	else {
-          size_t ts = Vector_size(temp);
-          size_t i=0;
-          int ret = 0;
-          for(i=0;i<ts;i++) {
-            if(Vector_get(temp, i)!=NULL) {
-	      ret = system(Vector_get(temp, i));
-	      if(ret!=0) {
-	        break;
-	      }
+        while (waiting_for_ready_target) {
+            omp_set_lock(&n[0]);
+            if (vector_size(task_vector) == 0) {
+                no_more_task = 1;
+                new_changes_in_dependencies = 1;
+                waiting_for_ready_target = 0;
             }
-          }
-          if(ret!=0) {
-	    r->state = -1;
-	    //pthread_mutex_lock(&m);
-	    queue_push(ruleq, (void*)r);
-            targetcount--;
-            //pthread_mutex_unlock(&m);
-          }
-          else {
-	    r->state = 1;
-	    //pthread_mutex_lock(&m);
-	    queue_push(ruleq, (void*)r);
-	    targetcount--;
-	    //pthread_mutex_unlock(&m);
-          }
-        }
-      }
-      else {
-        size_t i=0;
-        int invalid = 0;
-        for(i=0;i<Vector_size(r->dependencies);i++) {
-	  if(((rule_t*)Vector_get(r->dependencies,i))->state==-1) {
-	    r->state = -1;
-	    //pthread_mutex_lock(&m);
-	    queue_push(ruleq, (void*)r);
-	    targetcount--;
-	    invalid = 1;
-	    //pthread_mutex_unlock(&m);
-	    break;
-	  }
-	  if(Vector_get(r->dependencies,i)!=NULL && ((rule_t*)Vector_get(r->dependencies,i))->state==0) {
-	    invalid = 1;
-	    //pthread_mutex_lock(&m);
-	    queue_push(ruleq, (void*)r);//push back the rule since dependencies
-	    //pthread_mutex_unlock(&m);
-	    break;
-	  }
+            else {
+                size_t i = 0;
+                for (; i < vector_size(task_vector); i++) {
+                    char *temp_target = (char *)vector_get(task_vector, i);
+                    if ((size_t)(((rule_t *)graph_get_vertex_value(file_graph, temp_target))->data) == 0) {
+                        curr_target = strdup(temp_target);
+                        waiting_for_ready_target = 0;
+                        break;
+                    }
+                }
+
+                if (!waiting_for_ready_target) {
+                    vector_erase(task_vector, i);
+                    new_changes_in_dependencies = 1;
+                }
+                else {
+                    new_changes_in_dependencies = 0;
+                }
+            }
+            while (!new_changes_in_dependencies) pthread_cond_wait(&cu, &n[0]);
+            omp_unset_lock(&n[0]);
         }
 
-        if(invalid==0) {
-	  Vector* temp = r->commands;
-	  int needrun = 0;
-	  if(access(r->target,F_OK)==0) {
-            struct stat s;
-            struct stat a;
-            stat(r->target, &a);
-            size_t i = 0;
-            for(i=0;i<Vector_size(r->dependencies);i++) {
-              rule_t * tt = (rule_t*)Vector_get(r->dependencies,i);
-              stat(tt->target, &s);
-	      int dependaccess = access(tt->target,F_OK);
-              if(s.st_mtime - a.st_mtime > 0) {
-                needrun = 1;
+        if (no_more_task) break;
+
+        rule_t *curr_rule = (rule_t *)graph_get_vertex_value(file_graph, curr_target);
+        if (curr_rule->state != 0) {
+            change_antineighbor_data(curr_target);
+            continue;
+        }
+
+        vector *dependencies = graph_neighbors(file_graph, curr_target);
+
+        struct stat curr_st;
+        int is_file = access(curr_target, F_OK); // on success, return 0
+        if (is_file == 0) stat(curr_target, &curr_st);
+
+        int any_dependencies_failed = 0;
+        int have_non_file_dependencies = 0;
+        int have_newer_file_dependencies = 0;
+
+        for (size_t i = 0; i < vector_size(dependencies); i++) {
+            char *next_target = (char *)vector_get(dependencies, i);
+
+            if (access(next_target, F_OK) != 0) {
+                have_non_file_dependencies = 1;
+
+                rule_t *next_rule = (rule_t *)graph_get_vertex_value(file_graph, next_target);
+                omp_set_lock(&m[0]);
+                while (next_rule->state == 0) pthread_cond_wait(&cv, &m[0]);
+                if (next_rule->state == -1) any_dependencies_failed = 1;
+                omp_unset_lock(&m[0]);
+            }
+            else if (is_file == 0) {
+                struct stat dependency_st;
+                stat(next_target, &dependency_st);
+
+                if (difftime(curr_st.st_mtime, dependency_st.st_mtime) < 0) {
+                    have_newer_file_dependencies = 1;
+                }
+            }
+        }
+
+        vector_destroy(dependencies);
+
+        if (any_dependencies_failed) {
+            omp_set_lock(&m[0]);
+            curr_rule->state = -1;
+            pthread_cond_broadcast(&cv);
+            omp_unset_lock(&m[0]);
+
+            change_antineighbor_data(curr_target);
+            continue;
+        }
+
+        if (is_file == 0) {
+            if (!have_non_file_dependencies && !have_newer_file_dependencies) {
+                omp_set_lock(&m[0]);
+                curr_rule->state = 1;
+                pthread_cond_broadcast(&cv);
+                omp_unset_lock(&m[0]);
+
+                change_antineighbor_data(curr_target);
+                continue;
+            }
+        }
+
+        int any_commands_failed = 0;
+        for (size_t i = 0; i < vector_size(curr_rule->commands); i++) {
+            if (system(vector_get(curr_rule->commands, i)) != 0) {
+                any_commands_failed = 1;
                 break;
-              }
-	      if(dependaccess!=0) {
-		needrun = 1;
-		break;
-	      }
             }
-	    if(needrun==0) {
-	      r->state = 1;
-              //pthread_mutex_lock(&m);
-              queue_push(ruleq, (void*)r);
-              targetcount--;
-              //pthread_mutex_unlock(&m);
-	    }
-	  }
-	  if(access(r->target,F_OK)!=0 || needrun == 1) {
-	    size_t ts = Vector_size(temp);
-	    size_t i=0;
-	    int ret = 0;
-	    for(i=0;i<ts;i++) {
-	      if(Vector_get(temp, i)!=NULL) {
-	        ret = system(Vector_get(temp, i));
-	        if(ret!=0) {
-	          break;
-	        }
-	      }
-            }
-	    if(ret!=0) {
-	      r->state = -1;
-	      //pthread_mutex_lock(&m);
-	      queue_push(ruleq, (void*)r);
-	      targetcount--;
-	      //pthread_mutex_unlock(&m);
-	    }
-	    else {
-	      r->state = 1;
-	      //pthread_mutex_lock(&m);
-	      queue_push(ruleq, (void*)r);
-	      targetcount--;
-	      //pthread_mutex_unlock(&m);
-	    }
-          }
         }
-      }
+
+        if (any_commands_failed) {
+            omp_set_lock(&m[0]);
+            curr_rule->state = -1;
+            pthread_cond_broadcast(&cv);
+            omp_unset_lock(&m[0]);
+        }
+        else {
+            omp_set_lock(&m[0]);
+            curr_rule->state = 1;
+            pthread_cond_broadcast(&cv);
+            omp_unset_lock(&m[0]);
+        }
+        change_antineighbor_data(curr_target);
     }
-  }
-  return NULL;
+
+    return NULL;
 }
 
-int parmake(int argc, char **argv) {
-  int a=-1;
-  char * fvalue = NULL;
-  char * jvalue = NULL;
-  int numthreads = 1;
-  while((a=getopt(argc,argv,"f:j:")) != -1) {
-    switch(a) {
-      case 'f':
-        fvalue = optarg;
-        break;
-      case 'j':
-        jvalue = optarg;
-	numthreads = atoi(jvalue);
-        break;
-      default:
-	break;
-    }
-  }
-  FILE * f;
-  if(fvalue!=NULL) {
-    f = fopen(fvalue,"r");
-    if(f==NULL) {
-      exit(-1);
-    }
-  }
-  else {
-    f = fopen("./makefile","r");
-    if(f==NULL) {
-      f = fopen("./Makefile","r");
-      if(f==NULL) {
-        exit(-1);
-      }
-      else {
-        fvalue = "./Makefile";
-      }
-    }
-    else {
-      fvalue = "./makefile";
-    }
-  }
-  if(jvalue==NULL) {
-    numthreads = 1;
-  }
-  fclose(f);
+void parallel_makefile(size_t num_threads) {
+    // pthread_mutex_init(&m, NULL);
+    // pthread_mutex_init(&n, NULL);
+    m = (omp_lock_t *) malloc(1 * sizeof(omp_lock_t));
+    n = (omp_lock_t *) malloc(1 * sizeof(omp_lock_t));
+    omp_init_lock(&m[0]);
+    omp_init_lock(&n[0]);
+    pthread_cond_init(&cv, NULL);
+    pthread_cond_init(&cu, NULL);
 
-  v = Vector_create(mycopy, mydes);
-  circular = Vector_create(mycopy, mydes);
-  char ** targets = argv+optind;
-  //pthread_t p[numthreads];
-  ruleq = queue_create(-1, mycopy, mydes);
-  if(targets==NULL) {
-    parser_parse_makefile(fvalue,NULL,parsed_new_target);
-  }
-  else {
-    parser_parse_makefile(fvalue,targets,parsed_new_target);
-  }
-  size_t j=0;
-  /*for(j=0;j<Vector_size(v);j++) {
-    //printf("the target in v is %s\n",((rule_t*)Vector_get(v, j))->target);
-  }*/
-  for(j=0;j<Vector_size(v);j++) {
-    rule_t * temp = Vector_get(v, j);
-    size_t k = 0;
-    size_t z = 0;
-    //printf("%s : ",temp->target);
-    for(k=0;k<Vector_size(temp->dependencies);k++) {
-      rule_t * temp1 = Vector_get(temp->dependencies,k);
-      //printf(" %s ",temp1->target);
-      for(z=0;z<Vector_size(temp1->dependencies);z++) {
-        rule_t * temp2 = Vector_get(temp1->dependencies,z);
-	//printf(" %s ",temp2->target);
-        if(strcmp(temp->target,temp2->target)==0) {
-	  Vector_append(circular, (void*)temp);
-	  Vector_append(circular, (void*)temp1);
+    pthread_t pthread_id[num_threads];
+    for (size_t i = 0; i < num_threads; i++) {
+        pthread_id[i] = i;
+        pthread_create(&pthread_id[i], NULL, run_makefile, NULL);
+    }
+
+    void *result[num_threads];
+    for (size_t i = 0; i < num_threads; i++) {
+        pthread_join(pthread_id[i], &result[i]);
+    }
+
+    // pthread_mutex_destroy(&m);
+    // pthread_mutex_destroy(&n);
+    pthread_cond_destroy(&cv);
+    pthread_cond_destroy(&cu);
+}
+
+int parmake(char *makefile, size_t num_threads, char **targets) {
+    // good luck!
+    file_graph = parser_parse_makefile(makefile, targets);
+    vector *build_targets = graph_neighbors(file_graph, "");
+
+    for (size_t i = 0; i < vector_size(build_targets); i++) {
+        char *curr_target = (char *)vector_get(build_targets, i);
+        cycled_set = string_set_create();
+        task_set = string_set_create();
+
+        int have_cycle = detect_cycle(curr_target);
+        if (!have_cycle) {
+            task_vector = set_elements(task_set);
+
+            parallel_makefile(num_threads);
+
+            vector_destroy(task_vector);
         }
-      }
+        else {
+            print_cycle_failure(curr_target);
+        }
+
+        set_destroy(cycled_set);
+        set_destroy(task_set);
     }
-    //printf("\n");
-  }
-  /*for(j=0;j<Vector_size(circular);j++) {
-    printf("the target in circular is %s\n",((rule_t*)Vector_get(circular, j))->target);
-  }*/
-  // int i=0;
-  int * id = malloc(numthreads* sizeof(int));
 
-  
-  //for(i=0;i<numthreads;i++) {
-  //  id[i] = i+1;
-  //  pthread_create(&p[i], NULL, thread_function, (void*)&id[i]);
-  //}
-
-  //for(i=0;i<numthreads;i++) {
-  //  pthread_join(p[i],NULL);
-  //}
-  queue_destroy(ruleq);
-  free(id);
-  return 0;
+    vector_destroy(build_targets);
+    graph_destroy(file_graph);
+    return 0;
 }
